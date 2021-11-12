c2bf9745f047c4d915ae91303addf0749fe30b4d
VictorBarros <victorbarros1130@gmail.com>
2021-11-12 01:20:26 -0300

    integration

--- server/src/controller/Tweet.ts
+++ server/src/controller/Tweet.ts
@@ -1,22 +1,24 @@
 import { Request, Response } from 'express'
 import httpStatus from "http-status"
+import FinnHubService from '../services/Finnhub'
 import TwitterService from '../services/Twitter'
 
 export default {
   async postStock(req: Request, res: Response) {
-    // TODO authorization middleware
     const now = new Date()
 
-    const { symbol, price } = { symbol: "URA", price: 30.89 } // TODO fetch from vendor
+    // TODO add trycatch
+    const { symbol, price } = await FinnHubService.getQuoteRealTime()
 
     const message = [
       symbol,
-      `USD ${price.toFixed(2)}`,
+      `USD ${price}`,
       now.toString(),
       // TODO idea: https://twitter.com/DolarBipolar/status/1458801696017113093
       // TODO add font/vendor
     ].join("\n")
 
+    // TODO add trycatch
     const { id } = await TwitterService.writeTweet(message)
 
     return res
