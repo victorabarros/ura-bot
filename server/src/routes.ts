979968b8dbb19310bf2f8fb5b494157dfe656045
VictorBarros <victorbarros1130@gmail.com>
2021-11-19 01:49:17 -0300

    improve healthcheck

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import { Request, Response, Router } from "express"
 import httpStatus from "http-status"
 import Tweet from "./controller/Tweet"
+import FinHubService from "./services/Finnhub"
 import TwitterService from "./services/Twitter"
 import config from "./config"
 
@@ -10,14 +11,33 @@ const routes = Router()
 
 routes.post("/tweet", Tweet.postStock)
 routes.get("/health", async (req: Request, res: Response) => {
-  // TODO ping vendor
+  let responseStatus = httpStatus.OK
+  const services = {
+    finhub: {
+      success: true,
+    },
+    twitter: {
+      success: true,
+    },
+  }
+
+  await FinHubService.getQuoteRealTime()
+    .catch((err: unknown) => {
+      console.log(err)
+      responseStatus = httpStatus.SERVICE_UNAVAILABLE
+      services.finhub.success = false
+    })
 
   await TwitterService.check()
-  // TODO improve response
+    .catch((err: unknown) => {
+      console.log(err)
+      responseStatus = httpStatus.SERVICE_UNAVAILABLE
+      services.twitter.success = false
+    })
 
   return res
-    .status(httpStatus.OK)
-    .json({ health: "check", version })
+    .status(responseStatus)
+    .json({ services, version })
 })
 
 export default routes
