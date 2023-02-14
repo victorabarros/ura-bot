5f4e9186e9f56b364fc5e9cd0ea806fc34536e6b
VictorBarros <victorbarros1130@gmail.com>
2023-02-13 22:43:09 -0500

    refact

--- UraBotDotNet/UraBot.cs
+++ UraBotDotNet/UraBot.cs
@@ -9,19 +9,19 @@ namespace UraBotDotNet;
 
 public class UraBot
 {
-    public string[] StockSymbols = new string[]
-    {
-        "CCJ", "DNN", "NXE", "UEC", "URA", "URNM", "UUUU", "SRUUF", "PDN", "UROY",
-    };
+  public string[] StockSymbols = new string[]
+  {
+    "CCJ", "DNN", "NXE", "UEC", "URA", "URNM", "UUUU", "SRUUF", "PDN", "UROY",
+  };
 
-    [FunctionName("UraBot")]
-    public async Task Run([TimerTrigger("0 0 14-21 * * 1-5")]TimerInfo myTimer, ILogger log)
-    {
-        log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}, {myTimer}");
+  [FunctionName("UraBot")]
+  public async Task Run([TimerTrigger("0 0 14-21 * * 1-5")] TimerInfo myTimer, ILogger log)
+  {
+    log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}, {myTimer}");
 
-        var fin = new Finhub(log);
-        var quote = await fin.GetQuote("URA");
+    var fin = new Finhub(log);
+    var quote = await fin.GetQuote("URA");
 
-        log.LogInformation(quote.ToString());
+    log.LogInformation(quote.ToString());
   }
 }
