ad8921ace742523f3584c0ef1124ba5159fd6b16
VictorBarros <victorbarros1130@gmail.com>
2023-02-14 21:13:14 -0500

    fix identation

--- UraBotDotNet/Models/Quote.cs
+++ UraBotDotNet/Models/Quote.cs
@@ -1,27 +1,26 @@
 using UraBotDotNet.Services;
 
-namespace UraBotDotNet.Models
+namespace UraBotDotNet.Models;
+
+public class Quote
 {
-  public class Quote
+  public string symbol { get; set; }
+  public decimal price { get; set; }
+  public decimal highPrice { get; set; }
+  public decimal lowPrice { get; set; }
+  public decimal openPrice { get; set; }
+  public decimal previousClosePrice { get; set; }
+
+  public Quote()
   {
-    public string symbol { get; set; }
-    public decimal price { get; set; }
-    public decimal highPrice { get; set; }
-    public decimal lowPrice { get; set; }
-    public decimal openPrice { get; set; }
-    public decimal previousClosePrice { get; set; }
+  }
 
-    public Quote()
-    {
-    }
-
-    public Quote(GetQuoteResponse getQuoteResponse)
-    {
-      this.price = getQuoteResponse.c;
-      this.highPrice = getQuoteResponse.h;
-      this.lowPrice = getQuoteResponse.l;
-      this.openPrice = getQuoteResponse.o;
-      this.previousClosePrice = getQuoteResponse.pc;
-    }
+  public Quote(GetQuoteResponse getQuoteResponse)
+  {
+    this.price = getQuoteResponse.c;
+    this.highPrice = getQuoteResponse.h;
+    this.lowPrice = getQuoteResponse.l;
+    this.openPrice = getQuoteResponse.o;
+    this.previousClosePrice = getQuoteResponse.pc;
   }
 }
