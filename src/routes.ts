a79a216a4587a2ba273598390d74c8da9ec02c4f
VictorBarros <victorbarros1130@gmail.com>
2023-04-15 13:39:01 -0400

    improve log error

--- src/routes.ts
+++ src/routes.ts
@@ -34,21 +34,21 @@ routes.get("/health", async (req: Request, res: Response) => {
 
   await FinHubService.getQuoteRealTime("URA")
     .catch((err: unknown) => {
-      console.log(err)
+      console.error(JSON.stringify(err))
       responseStatus = httpStatus.SERVICE_UNAVAILABLE
       services.finhub.success = false
     })
 
   await UraTwitterService.check()
     .catch((err: unknown) => {
-      console.log(err)
+      console.error(JSON.stringify(err))
       responseStatus = httpStatus.SERVICE_UNAVAILABLE
       services.twitter.ura.success = false
     })
 
   await BrlTwitterService.check()
     .catch((err: unknown) => {
-      console.log(err)
+      console.error(JSON.stringify(err))
       responseStatus = httpStatus.SERVICE_UNAVAILABLE
       services.twitter.brl.success = false
     })
