e21891de37470949e1db4cad7c356c73fd01eaae
Victor Barros <victorbarros1130@gmail.com>
2024-07-09 12:27:56 -0400

    add todo

--- src/controller/BrazilianReal.ts
+++ src/controller/BrazilianReal.ts
@@ -21,7 +21,7 @@ export const postBrlPrice = async (req: Request, res: Response) => {
     .concat(
       ["usd", "eur", "cad", "gbp", "chf", "jpy", "btc"].map((c: string) => {
         const currency = (currencies as any)[c]
-        return `${currency.flag} $${currency.symbol} ${(currency.value * currencies.brl.value).toFixed(2)}`
+        return `${currency.flag} $${currency.symbol} ${(currency.value * currencies.brl.value).toFixed(2)}` // TODO pretify number, like 313759.29 to 313,759.29
       })
     ).concat(["\n", signature(now, "#BRLbot"),])
 
--- src/services/Currency.ts
+++ src/services/Currency.ts
@@ -38,7 +38,7 @@ export class CurrencyService implements ICurrencyService {
       gbp: { value: parseFloat(rates.GBP), symbol: "GBP", name: "Pound Britanico", flag: "🇬🇧" },
       chf: { value: parseFloat(rates.CHF), symbol: "CHF", name: "Franco Suiço", flag: "🇨🇭" },
       cad: { value: parseFloat(rates.CAD), symbol: "CAD", name: "Dolar Canadense", flag: "🇨🇦" },
-      btc: { value: 1/parseFloat(rates.BTC), symbol: "BTC", name: "Bitcoin", flag: "🍕" },
+      btc: { value: 1/parseFloat(rates.BTC), symbol: "BTC", name: "Bitcoin", flag: "#Bitcoin" },
     }
   }
 
